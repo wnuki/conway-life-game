1a9e24f29029a3e85a9f50592f2b42843768829b
Kinga Wnuk <kinga.wnuk@protonmail.com>
2019-09-29 23:23:54 +0200

    have main class

---
+++ src/app/App.java
@@ -0,0 +1,24 @@
+package app;
+
+import javax.swing.*;
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+public class App {
+
+
+    public static void main(String[] args) throws IOException, URISyntaxException {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException | InstantiationException| IllegalAccessException | UnsupportedLookAndFeelException e) {
+            e.printStackTrace();
+        }
+
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new MainFrame();
+            }
+        });
+    }
+}
