5142ed83d0a4ad12eaca818ab35f9540ec2da90a
Kinga Wnuk <kinga.wnuk@protonmail.com>
2019-09-29 23:32:14 +0200

    constants encapsulation

---
+++ src/constants/Constants.java
@@ -0,0 +1,24 @@
+package constants;
+
+public class Constants {
+    private Constants() {
+    }
+
+    public static final String NAME_OF_APPLICATION = "Conway's Game";
+    public static final int NUMBER_OF_ROWS = 30;
+    public static final int NUMBER_OF_COLUMS = 30;
+    public static final String START = "Start";
+    public static final String RESTART = "Reset";
+    public static final int TIME_LAG = 200;
+
+    public static final int FRAME_WIDTH = 700;
+    public static final int FRAME_HEIGHT = 700;
+
+    public static final String GREEN_COLOR = "#01a900";
+
+    public static final String MENU_FILE = "File";
+    public static final String MENU_EXIT = "Exit";
+    public static final String MENU_ABOUT = "About";
+    public static final String MENU_EXIT_TEXT = "Do you want to exit?";
+    public static final String MENU_EXIT_TITLE = "Warning";
+}
